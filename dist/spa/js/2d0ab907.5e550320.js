81c34ec65f3816a8b987dbee2c7d75ccdb777d89
phoenix-yjb <phoenix.yjb@gmail.com>
2019-08-26 09:52:49 +0100

    add the first public frontend

---
+++ dist/spa/js/2d0ab907.5e550320.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["2d0ab907"],{"168d":function(e,n,a){"use strict";a.r(n);var s=function(){var e=this,n=e.$createElement,a=e._self._c||n;return a("q-page",{staticClass:"flex flex-center"},[e._v("\n  This is membership page\n")])},t=[],l={name:"PageIndex"},c=l,i=a("2877"),r=Object(i["a"])(c,s,t,!1,null,null,null);n["default"]=r.exports}}]);
---
+++ dist/spa/js/2d209afc.c8b967e0.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["2d209afc"],{a9b0:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("q-page",{staticClass:"flex flex-center"},[e._v("\n  This is Mentor home\n")])},s=[],l={name:"PageIndex"},c=l,r=t("2877"),i=Object(r["a"])(c,a,s,!1,null,null,null);n["default"]=i.exports}}]);
